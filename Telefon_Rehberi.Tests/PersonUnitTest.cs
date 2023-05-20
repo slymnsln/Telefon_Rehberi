d79e6ae3b2ca57f73411aecb9ca117e8b49f0816
SuLeYMaNN <suleymansulun@outlook.com>
2023-05-21 02:16:43 +0300

    Kişi ekleme testi

---
+++ Telefon_Rehberi.Tests/PersonUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using Telefon_Rehberi.Business.Concrete;
+using Telefon_Rehberi.DataAccess.Concrete;
+using Telefon_Rehberi.Entities.Concrete;
+
+namespace Telefon_Rehberi.Tests
+{
+    public class PersonUnitTest
+    {
+        [Fact]
+        public void PersonAdd()
+        {
+            var personManager = new PersonManager(new EfPersonDal());
+
+            var person = new Person
+            {
+                FirstName = "TestFirstName1",
+                LastName = "TestLastName1",
+                CompanyName = "TestCompanyName1"
+            };
+
+            var result = personManager.Add(person);
+
+            Assert.True(result.Success);
+        }
+    }
+}
