c3f5e79abdd0a1573427264a8c143d27fda1a602
SuLeYMaNN <suleymansulun@outlook.com>
2023-05-21 01:26:09 +0300

    Inteface eklemeler yapıldı

---
+++ Telefon_Rehberi.Business/Abstract/IContactInformationService.cs
@@ -0,0 +1,12 @@
+﻿using Telefon_Rehberi.Core.Utilities.Results;
+using Telefon_Rehberi.Entities.Concrete;
+
+namespace Telefon_Rehberi.Business.Abstract
+{
+    public interface IContactInformationService
+    {
+        IResult Add(ContactInformation contactInformation);
+        IResult Delete(int contactId);
+        IDataResult<IList<ContactInformation>> GetListByPersonId(int personId);
+    }
+}
---
+++ Telefon_Rehberi.Business/Abstract/IReportService.cs
@@ -0,0 +1,13 @@
+﻿using Telefon_Rehberi.Core.Utilities.Results;
+using Telefon_Rehberi.Entities.Concrete;
+
+namespace Telefon_Rehberi.Business.Abstract
+{
+    public interface IReportService
+    {
+        Report Add(Report report);
+        IResult Update(Report report);
+        IDataResult<Report> GetById(int reportId);
+        IDataResult<IList<Report>> GetAll();
+    }
+}
