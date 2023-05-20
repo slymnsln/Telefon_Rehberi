8ab4eb6c023080bb924adff9b0bfbad0cb9f3b6f
SuLeYMaNN <suleymansulun@outlook.com>
2023-05-21 01:24:30 +0300

    Inteface'ler oluşturuldu

---
+++ Telefon_Rehberi.Business/Abstract/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using Telefon_Rehberi.Core.Utilities.Results;
+using Telefon_Rehberi.Entities.Concrete;
+using Telefon_Rehberi.Entities.DTOs;
+
+namespace Telefon_Rehberi.Business.Abstract
+{
+    public interface IPersonService
+    {
+        IResult Add(Person person);
+        IResult Delete(int personId);
+        IDataResult<IList<Person>> GetAll();
+        IList<ReportByLocationDto> GetLocationAllReports();
+    }
+}
