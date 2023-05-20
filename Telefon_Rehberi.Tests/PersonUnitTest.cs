2d332246a8d32ab4fa641809f897ba4ef8870b89
SuLeYMaNN <suleymansulun@outlook.com>
2023-05-21 02:43:11 +0300

    Kişileri listeleme ve Location rapor tesleri yapıldı

--- Telefon_Rehberi.Tests/PersonUnitTest.cs
+++ Telefon_Rehberi.Tests/PersonUnitTest.cs
@@ -1,6 +1,8 @@
 ﻿using Telefon_Rehberi.Business.Concrete;
+using Telefon_Rehberi.Core.Utilities.Results;
 using Telefon_Rehberi.DataAccess.Concrete;
 using Telefon_Rehberi.Entities.Concrete;
+using Telefon_Rehberi.Entities.DTOs;
 
 namespace Telefon_Rehberi.Tests
 {
@@ -13,9 +15,9 @@ namespace Telefon_Rehberi.Tests
 
             var person = new Person
             {
-                FirstName = "TestFirstName2",
-                LastName = "TestLastName2",
-                CompanyName = "TestCompanyName2"
+                FirstName = "TestFirstName4",
+                LastName = "TestLastName4",
+                CompanyName = "TestCompanyName4"
             };
 
             var result = personManager.Add(person);
@@ -33,6 +35,27 @@ namespace Telefon_Rehberi.Tests
             var result = personManager.Delete(personId);
 
             Assert.True(result.Success);
+        }
+
+        [Fact]
+        public void GetList()
+        {
+            var personManager = new PersonManager(new EfPersonDal());
+
+            var result = personManager.GetAll();
+
+            Assert.IsType<List<Person>>(result.Data);
+            Assert.True(result.Success);
+        }
+
+        [Fact]
+        public void GetLocationAllReports()
+        {
+            var personManager = new PersonManager(new EfPersonDal());
+
+            var result = personManager.GetLocationAllReports();
+
+            Assert.IsType<List<ReportByLocationDto>>(result);
         }
     }
 }
