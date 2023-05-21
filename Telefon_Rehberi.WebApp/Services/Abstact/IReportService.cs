119ad30db292e97c00c42bc07fb20703e72d5c32
SuLeYMaNN <suleymansulun@outlook.com>
2023-05-21 04:27:22 +0300

    Service interfaceleri oluşturuldu.

---
+++ Telefon_Rehberi.WebApp/Services/Abstact/IReportService.cs
@@ -0,0 +1,11 @@
+﻿using Telefon_Rehberi.WebApp.Models.ServiceModel;
+
+namespace Telefon_Rehberi.WebApp.Services.Abstact
+{
+    public interface IReportService
+    {
+        bool RequestReport();
+        ResponseDataModel<List<Report>> GetAll();
+        byte[] ReportDownload(int reportId);
+    }
+}
