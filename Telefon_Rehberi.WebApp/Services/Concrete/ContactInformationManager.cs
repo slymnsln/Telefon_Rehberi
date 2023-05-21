abccc8095d6491670b9287010fa223038b2db112
SuLeYMaNN <suleymansulun@outlook.com>
2023-05-21 04:33:34 +0300

    Concrete da Manager claslar'ı oluşturuldu

---
+++ Telefon_Rehberi.WebApp/Services/Concrete/ContactInformationManager.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using Telefon_Rehberi.WebApp.Models;
+using Telefon_Rehberi.WebApp.Models.ServiceModel;
+using Telefon_Rehberi.WebApp.Services.Abstact;
+
+namespace Telefon_Rehberi.WebApp.Services.Concrete
+{
+    public class ContactInformationManager : IContactInformationService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+        string apiUrl;
+
+        public ContactInformationManager(IConfiguration configuration)
+        {
+            _configuration= configuration;
+        }
+
+        public ResponseModel Add(ContactInformationViewModel contactInformationViewModel)
+        {
+            var result = new ResponseModel { Success = false };
+
+            HttpContent body = new StringContent(JsonConvert.SerializeObject(contactInformationViewModel), Encoding.UTF8, "application/json");
+            HttpResponseMessage httpResponseMessage = _httpClient.PostAsync($"{apiUrl}/Add", body).Result;
+            if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonData = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                result = JsonConvert.DeserializeObject<ResponseModel>(jsonData);
+            }
+            return result;
+        }
+
+        public ResponseModel Delete(int contactInformationId)
+        {
+            var result = new ResponseModel { Success = false };
+
+            HttpResponseMessage httpResponseMessage = _httpClient.DeleteAsync($"{apiUrl}/Delete?contactInformationId={contactInformationId}").Result;
+            if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonData = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                result = JsonConvert.DeserializeObject<ResponseModel>(jsonData);
+            }
+            return result;
+        }
+
+        public ResponseDataModel<List<ContactInformation>> GetAllByPersonId(int personId)
+        {
+            var result = new ResponseDataModel<List<ContactInformation>>() { Success = false };
+            HttpResponseMessage httpResponseMessage = _httpClient.GetAsync($"{apiUrl}/GetByPersonId?personId={personId}").Result;
+            if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonData = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                result = JsonConvert.DeserializeObject<ResponseDataModel<List<ContactInformation>>>(jsonData);
+            }
+            return result;
+        }
+    }
+}
---
+++ Telefon_Rehberi.WebApp/Services/Concrete/ReportManager.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using Telefon_Rehberi.WebApp.Models.ServiceModel;
+using Telefon_Rehberi.WebApp.Services.Abstact;
+
+namespace Telefon_Rehberi.WebApp.Services.Concrete
+{
+    public class ReportManager : IReportService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+        string apiUrl;
+        public ReportManager(IConfiguration configuration)
+        {
+            _configuration= configuration;
+        }
+
+        public ResponseDataModel<List<Report>> GetAll()
+        {
+            var result = new ResponseDataModel<List<Report>>() { Success = false };
+            HttpResponseMessage httpResponseMessage = _httpClient.GetAsync($"{apiUrl}/GetAll").Result;
+            if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonData = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                result = JsonConvert.DeserializeObject<ResponseDataModel<List<Report>>>(jsonData);
+            }
+            return result;
+        }
+
+        public byte[] ReportDownload(int reportId)
+        {
+            HttpResponseMessage httpResponseMessage = _httpClient.PostAsync($"{apiUrl}/ReportDownload?reportId={reportId}", null).Result;
+            var file = httpResponseMessage.Content.ReadAsByteArrayAsync().Result;
+            return file;
+        }
+
+        public bool RequestReport()
+        {
+            var result = new ResponseDataModel<List<Report>>() { Success = false };
+            HttpResponseMessage httpResponseMessage = _httpClient.GetAsync($"{apiUrl}/RequestReport").Result;
+            if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
